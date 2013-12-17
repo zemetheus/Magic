375574360e9f67e57f5ddf1f00336a6b22c1b5a8
Robert Cotrell <rcotrell3@gmail.com>
2013-12-17 18:09:02 -0500

    AddCardWindow change: JComboBox for Card Type

--- src/AddCardWindow.java
+++ src/AddCardWindow.java
@@ -4,54 +4,24 @@ import javax.swing.*;
 
 public class AddCardWindow extends JFrame
 {
-	JButton addButton, cancelButton;
+	String[] cardTypes = {"Instant", "Sorcery", "Enchantment", "Artifact", "Creature", "Land"};
+	JComboBox cardTypeBox = new JComboBox(cardTypes);
 	
 		
 	public AddCardWindow()
 	{
-		super("AddCardWindow");
+		super("Add a Card");
 		
-		addButton.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				
-			}
-		});
-		
-		cancelButton.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				setVisible(false);
-			}
-		});
-		
-		JMenuItem cardType1 = new JMenuItem("Instant");
-		JMenuItem cardType2 = new JMenuItem("Sorcery");
-		JMenuItem cardType3 = new JMenuItem("Enchantment");
-		JMenuItem cardType4 = new JMenuItem("Artifact");
-		JMenuItem cardType5 = new JMenuItem("Creature");
-		JMenuItem cardType6 = new JMenuItem("Land");
-		
-		JMenuBar cardTypeMenuBar = new JMenuBar();
-		JMenu cardTypeMenu = new JMenu("Card Type");
-		
-		cardTypeMenu.add(cardType1);
-		cardTypeMenu.add(cardType2);
-		cardTypeMenu.add(cardType3);
-		cardTypeMenu.add(cardType4);
-		cardTypeMenu.add(cardType5);
-		cardTypeMenu.add(cardType6);
-		
-		cardTypeMenuBar.add(cardTypeMenu);
-		
-		JTextArea edit = new JTextArea(8, 40);
-		BorderLayout bord = new BorderLayout();
-		setLayout(bord);
-		setJMenuBar(cardTypeMenuBar);
-		pack();
+		setSize(220, 150);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel pane = new JPanel();
+		JLabel cardTypeLabel = new JLabel("Select a card type: ");
+		pane.add(cardTypeLabel);
+		pane.add(cardTypeBox);
+		add(pane);
 		setVisible(true);
+		
+		
 		
 	}
 	
